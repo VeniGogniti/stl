9c9c0184461cd129365ee1b2bfad93239769e9f0
VeniGogniti <stefx@op.pl>
2024-07-12 13:47:11 +0200

    remove-vowels

--- homework/remove-vowels/vowel.hpp
+++ homework/remove-vowels/vowel.hpp
@@ -1,8 +1,8 @@
 #pragma once
 
-#include<algorithm>
-#include<iostream>
-#include<string>
-#include<vector>
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <vector>
 
 std::vector<std::string> removeVowels(std::vector<std::string>& newVowels);
