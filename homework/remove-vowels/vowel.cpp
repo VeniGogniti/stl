1a6da324ef7361547378df09de0701d2a36afc4d
VeniGogniti <stefx@op.pl>
2024-07-12 13:42:26 +0200

    remove-vowels

--- homework/remove-vowels/vowel.cpp
+++ homework/remove-vowels/vowel.cpp
@@ -5,8 +5,9 @@ std::vector<std::string> removeVowels(std::vector<std::string>& newVowels){
 
     for (auto& it : newVowels) {
         it.erase(std::remove_if(it.begin(), it.end(), [](char c) {
-            return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || c == 'y' || c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U' || c == 'Y';
-        }), it.end());
+                    return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || c == 'y' || c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U' || c == 'Y';
+                }),
+                it.end());
     }
     return newVowels;
 
