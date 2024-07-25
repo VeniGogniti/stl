ae231162187a938fa6bbc3ea775bdebaec32e7bc
VeniGogniti <stefx@op.pl>
2024-07-25 16:14:35 +0200

    grayscale-image

--- homework/grayscale-image/compression.cpp
+++ homework/grayscale-image/compression.cpp
@@ -7,8 +7,8 @@
 #include <vector>
 #include <cstddef>
 
-constexpr size_t width = 32;
-constexpr size_t height = 32;
+//constexpr size_t width = 32;
+//constexpr size_t height = 32;
 
 std::vector<std::pair<uint8_t, uint8_t>> compressGrayscale(std::array<std::array<uint8_t, width>, height>& myFunction) {
     std::vector<std::pair<uint8_t, uint8_t>> result;
