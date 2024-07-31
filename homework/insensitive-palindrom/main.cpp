e1291d6c7f9d8fcf237e7da4314189e1982c7069
VeniGogniti <stefx@op.pl>
2024-07-31 13:04:50 +0200

    insensitive-palindrom

--- homework/insensitive-palindrom/main.cpp
+++ homework/insensitive-palindrom/main.cpp
@@ -6,10 +6,8 @@ bool is_palindrome(std::string s) {
     std::transform(s.begin(), s.end(), s.begin(), ::tolower);
     s.erase(std::remove_if(s.begin(), s.end(), [](char c) {
                 return !std::isalnum(c);
-           
             }),
             s.end());
-    
     std::string reversed = s;
     std::reverse(reversed.begin(), reversed.end());
 
