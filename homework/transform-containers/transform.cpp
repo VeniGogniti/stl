bb75ab373b451fee5e0a1944c3fd2a1ff8baff90
VeniGogniti <stefx@op.pl>
2024-07-31 15:12:31 +0200

    transform-container

---
+++ homework/transform-containers/transform.cpp
@@ -0,0 +1,16 @@
+#include "transform.hpp"
+
+std::map<int, std::string> removeDuplicateAndTranformToMap(std::list<std::string> list, std::deque<int> deq) {
+    list.sort();
+    list.unique();
+
+    std::sort(deq.begin(), deq.end());
+    deq.erase(std::unique(deq.begin(), deq.end()), deq.end());
+
+    std::map<int, std::string> result;
+    auto iter = list.begin();
+    std::transform(deq.begin(), deq.end(), std::inserter(result, result.end()), [&](auto i){
+            return std::make_pair(i, *iter++);});
+
+    return result; 
+}
---
+++ homework/transform-containers/transform.hpp
@@ -0,0 +1,8 @@
+#pragma once
+#include <algorithm>
+#include <deque>
+#include <list>
+#include <map>
+#include <string>
+#include <vector>
+std::map<int, std::string> removeDuplicateAndTranformToMap(std::list<std::string> list, std::deque<int> deq);
