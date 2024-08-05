df26dbacc812811fdc12c22bf5cb16b3e0001c8e
VeniGogniti <stefx@op.pl>
2024-08-05 17:44:47 +0200

    inner-product

--- homework/inner-product/arithmeticAverage.cpp
+++ homework/inner-product/arithmeticAverage.cpp
@@ -8,7 +8,7 @@
 double ArithmeticAverage(std::vector<int> first, std::vector<int> second) {
     std::vector<double> result(first.size());
     std::transform(first.begin(), first.end(), second.begin(), result.begin(),
-                   [](auto it1, auto it2){return static_cast<double> (it1 + it2); });
+                   [](auto it1, auto it2) { return static_cast<double>(it1 + it2); });
 
     double total_result = std::accumulate(result.begin(), result.end(), 0.0);
 
@@ -20,7 +20,7 @@ double ArithmeticAverage(std::vector<int> first, std::vector<int> second) {
 double Distance(std::vector<int> first, std::vector<int> second) {
     std::vector<double> result(first.size());
     std::transform(first.begin(), first.end(), second.begin(), result.begin(),
-                   [](auto it1, auto it2){return std::pow(it1 - it2, 2);});
+                   [](auto it1, auto it2) { return std::pow(it1 - it2, 2); });
 
     double range = std::accumulate(result.begin(), result.end(), 0.0);
     return std::sqrt(range);
