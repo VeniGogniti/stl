318f4dea0f7fb3db571426352949db55f91451f5
VeniGogniti <stefx@op.pl>
2024-08-05 17:42:18 +0200

    inner-product

--- homework/inner-product/main.cpp
+++ homework/inner-product/main.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 #include "arithmeticAverage.hpp"
 
-int main () {
+int main() {
     std::vector<int> first;
     std::vector<int> second;
 
     ArithmeticAverage(first, second);
     Distance(first, second);
-return 0;
+    return 0;
 }
